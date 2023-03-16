92166c800aa5981e052c24f6e0d2014f873e7eb3
xwytdryms <muftifz16@gmail.com>
2023-03-16 13:55:21 +0700

    nambah list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, vitae quaerat? Laboriosam provident necessitatibus, pariatur minima, alias incidunt praesentium voluptatem ratione, sed autem cupiditate earum non? Ipsam amet facere et?</p>
+<ul>
+    <li>tea</li>
+    <li>coffee</li>
+    <li>milk</li>
+</ul>
 </body>
 </html>
